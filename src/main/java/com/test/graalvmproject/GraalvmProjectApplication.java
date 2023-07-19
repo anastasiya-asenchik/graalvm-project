54070c352d16d530d0f6856d96768c96ae13bafa
Anastasiya Asenchik <a.asenchik@godeltech.com>
2023-07-20 01:29:20 +0200

    Initial commit

---
+++ src/main/java/com/test/graalvmproject/GraalvmProjectApplication.java
@@ -0,0 +1,13 @@
+package com.test.graalvmproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraalvmProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraalvmProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/graalvmproject/GraalvmProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.graalvmproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraalvmProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
